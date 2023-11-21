6fc01d62bc349a356844c9b2a5113c62503ee38f
mnithishkumar123 <125072337+mnithishkumar123@users.noreply.github.com>
2023-11-21 23:47:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const tls = document.getElementById('user-table');
-
-// Retrieving existing user data from local storage
-
 let entry = JSON.parse(localStorage.getItem('users')) || [];
-
-// Displaying existing user data in table
 for (const user of entry) {
     const {name, email, password, dob, terms} = user;
     const row = tls.insertRow();
@@ -14,20 +9,14 @@ for (const user of entry) {
     row.insertCell().textContent = dob;
     row.insertCell().textContent = terms ? 'true' : 'false';
 }
-
-// Handle form submit event
 const frs = document.getElementById('registration_form');
 frs.addEventListener('submit', (event) => {
     event.preventDefault();
-
-    // Getting the form data
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const dob = document.getElementById('dob').value;
     const terms = document.getElementById('terms').checked;
-
-    // Validating date of birth
     const d = new Date(dob);
     const n = new Date();
     const bfd = (new Date(n.getFullYear() - 55, n.getMonth(), n.getDate()))
@@ -38,8 +27,6 @@ frs.addEventListener('submit', (event) => {
         alert('You must be 18 years old to register');
         return;
     }
-
-    // Adding user to table and saving the data to local storage
     const user = {name, email, password, dob, terms};
     entry.push(user);
     localStorage.setItem('users', JSON.stringify(entry));
@@ -49,6 +36,5 @@ frs.addEventListener('submit', (event) => {
         const cell = row.insertCell();
         cell.textContent = item;
     })
-    // Reset form
     frs.reset();
 });
